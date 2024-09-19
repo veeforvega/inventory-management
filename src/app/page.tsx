f2ea9ec10f3562902ac2da008c7a9e2b0c8afd9e
Mikhail Vega <mikhailvega@Mikhails-MBP.attlocal.net>
2024-09-19 16:47:28 -0700

    setup dashboard components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
+import Dashboard from "@/app/dashboard/page"
 
 export default function Home() {
   return (
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Hello world
-      </main>
+      <Dashboard />
       
   );
 }
