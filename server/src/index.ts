d5c4925d61cd477153af7107c870f6de160c862b
Mikhail Vega <mikhailvega@Mikhails-MBP.attlocal.net>
2024-09-21 15:54:42 -0700

    backend libraries, dependencies; setup backend main file

---
+++ server/src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+//  Route imports
+
+// Conifigurations
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// Routes
+
+// Server
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
