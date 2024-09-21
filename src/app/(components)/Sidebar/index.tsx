7a12083673731e5b7dba8c6aba207581ed0a5e16
Mikhail Vega <mikhailvega@Mikhails-MBP.attlocal.net>
2024-09-20 18:08:15 -0700

    toggle functionality for sidebar

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,34 +1,49 @@
 "use client";
 
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed } from "@/state";
 import { Menu } from "lucide-react";
 import React from "react";
 
 const Sidebar = () => {
-  return (
-    <div>
-        {/* Top logo */}
-        <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
-            <div>
-                Logo
-            </div>
-            <h1 className="font-extrabold text-2xl">VEESTOCK</h1>
-            <button 
-                className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" 
-                onClick={()=> {}}>
-                <Menu className="w-4 h-4" />
-            </button>
-        </div>
-        {/* Links */}
-        <div className="flex-grow mt-8">
-            {/* links here */}
-        </div>
+    const dispatch = useAppDispatch();
+    const isSidebarCollapsed = useAppSelector(
+        (state) => state.global.isSidebarCollapsed
+      );
 
-        {/* Footer */}
-        <div>
-            <p className="text-center text-xs text-gray-500">&copy; 2024 Veestock</p>
+    const toggleSidebar = () => {
+        dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+    };
+
+    const sidebarClassNames = `fixed flex flex-col ${
+        isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
+        } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`;
+
+    return (
+        <div className={sidebarClassNames}>
+            {/* Top logo */}
+            <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+                <div>
+                    Logo
+                </div>
+                <h1 className="font-extrabold text-2xl">VEESTOCK</h1>
+                <button 
+                    className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" 
+                    onClick={toggleSidebar}>
+                    <Menu className="w-4 h-4" />
+                </button>
+            </div>
+            {/* Links */}
+            <div className="flex-grow mt-8">
+                {/* links here */}
+            </div>
+
+            {/* Footer */}
+            <div>
+                <p className="text-center text-xs text-gray-500">&copy; 2024 Veestock</p>
+            </div>
         </div>
-    </div>
-  )
+    )
 };
 
 export default Sidebar;
