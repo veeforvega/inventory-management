82e6252e430b9db92b56321e51003f9300cc8627
Mikhail Vega <mikhailvega@Mikhails-MBP.attlocal.net>
2024-09-20 18:54:39 -0700

    add light/dark mode toggle

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useAppDispatch, useAppSelector } from "@/app/redux";
-import { setIsSidebarCollapsed } from "@/state";
-import { Bell, Menu, Settings, Sun } from "lucide-react";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
+import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
@@ -11,9 +11,14 @@ const Navbar = () => {
     const isSidebarCollapsed = useAppSelector(
       (state) => state.global.isSidebarCollapsed
     );
+    const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
     const toggleSidebar = () => {
-      dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+        dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+    };
+
+    const toggleDarkMode = () => {
+        dispatch(setIsDarkMode(!isDarkMode));
     };
     
     return (
@@ -26,8 +31,8 @@ const Navbar = () => {
                 <div className="relative">
                     <input 
                         type="search" 
-                        placeholder="Searh groups & products" 
-                        className="pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500" />
+                        placeholder="Search groups & products" 
+                        className="pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500" />
                     
                     <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                         <Bell className="text-gray-500" size={20} />
@@ -37,8 +42,12 @@ const Navbar = () => {
             {/* Right side */}
             <div className="flex justify-between items-center gap-5">
                 <div className="hidden md:flex justify-between items-center gap-5">
-                    <button onClick={()=>{}}>
-                        <Sun className="cursor-pointer text-gray-500 size={24}" />
+                    <button onClick={toggleDarkMode}>
+                        {isDarkMode ? (
+                            <Sun className="cursor-pointer text-gray-500" size={24} />
+                        ) : (
+                            <Moon className="cursor-pointer text-gray-500" size={24} />
+                        )}
                     </button>
                 </div>
                 <div className="relative">
