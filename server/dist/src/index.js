77aa2582a3d42cfe8bcdcfbd8f92baa4389ad9dc
Mikhail Vega <mikhailvega@Mikhails-MBP.attlocal.net>
2024-09-21 18:14:26 -0700

    create dashboard route

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -10,6 +10,7 @@ const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
 //  Route imports
+const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
 // Conifigurations
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -21,6 +22,7 @@ app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use((0, cors_1.default)());
 // Routes
+app.use("/dashboard", dashboardRoutes_1.default); // http://localhost:8000/dashboard
 // Server
 const port = Number(process.env.PORT) || 3001;
 app.listen(port, () => {
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,9 +4,8 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-
 //  Route imports
-
+import dashboardRoutes from "./routes/dashboardRoutes";
 // Conifigurations
 dotenv.config();
 const app = express();
@@ -20,6 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // Routes
+app.use("/dashboard", dashboardRoutes); // http://localhost:8000/dashboard
 
 // Server
 const port = Number(process.env.PORT) || 3001;
