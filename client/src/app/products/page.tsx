3bf202e99e82d68b05cc475a7969ac46cc73f9ba
Mikhail Vega <mikhailvega@Mikhails-MBP.attlocal.net>
2024-09-25 16:21:30 -0700

    search bar and modal button for products page

--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { useGetProductsQuery } from "@/state/api";
+import { PlusCircleIcon, SearchIcon } from "lucide-react";
 import { useState } from "react";
+import Header from "@/app/(components)/Header";
 
 const Products = () => {
     const [searchTerm, setSearchTerm] = useState("");
@@ -27,7 +29,30 @@ const Products = () => {
 
     return (
         <div className="mx-auto pb-5 w-full">
-            Products
+            {/* Search Bar */}
+            <div className="mb-6">
+                <div className="flex items-center border-2 border-gray-200 rounded">
+                <SearchIcon className="w-5 h-5 text-gray-500 m-2" />
+                <input
+                    className="w-full py-2 px-4 rounded bg-white"
+                    placeholder="Search products"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                </div>
+            </div>
+
+            {/* Header Bar */}
+            <div className="flex justify-between items-center mb-6">
+                <Header name="Products" />
+                <button
+                    className="flex items-center bg-blue-500 hover:bg-blue-700 text-gray-200 font-bold py-2 px-4 rounded"
+                    onClick={() => setIsModalOpen(true)}
+                >
+                    <PlusCircleIcon className="w-5 h-5 mr-2 !text-gray-200" /> 
+                    Create Product
+                </button>
+            </div>
         </div>
     )
 }
