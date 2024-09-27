17f0726956c95568a597981c0df50f0a740b24d8
Mikhail Vega <mikhailvega@Mikhails-MBP.attlocal.net>
2024-09-26 20:59:48 -0700

    expenses api && route

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -13,6 +13,7 @@ const morgan_1 = __importDefault(require("morgan"));
 const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
 const productRoutes_1 = __importDefault(require("./routes/productRoutes"));
 const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
+const expenseRoutes_1 = __importDefault(require("./routes/expenseRoutes"));
 // Conifigurations
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -27,6 +28,7 @@ app.use((0, cors_1.default)());
 app.use("/dashboard", dashboardRoutes_1.default);
 app.use("/products", productRoutes_1.default);
 app.use("/users", userRoutes_1.default);
+app.use("/expenses", expenseRoutes_1.default);
 // Server
 const port = Number(process.env.PORT) || 3001;
 app.listen(port, () => {
