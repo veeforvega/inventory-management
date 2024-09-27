8c55f94fa7ff32e7af4df19ade8cdf3b156cf0d1
Mikhail Vega <mikhailvega@Mikhails-MBP.attlocal.net>
2024-09-27 15:18:30 -0700

    expenses page

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -59,7 +59,7 @@ export interface User {
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
     reducerPath: "api",
-    tagTypes: ["DashboardMetrics", "Products", "Users"],
+    tagTypes: ["DashboardMetrics", "Products", "Users", "Expenses"],
     endpoints: (build) => (
         {
             getDashboardMetrics: build.query<DashboardMetrics, void>({
@@ -85,7 +85,10 @@ export const api = createApi({
                 query: () => "/users",
                 providesTags: ["Users"],
             }),
-            
+            getExpensesByCategory: build.query<ExpenseByCategorySummary[], void>({
+                query: () => "/expenses",
+                providesTags: ["Expenses"],
+            }),
         }
     )
 });
@@ -95,4 +98,5 @@ export const {
     useGetProductsQuery,
     useCreateProductMutation,
     useGetUsersQuery,
+    useGetExpensesByCategoryQuery,
 } = api;
